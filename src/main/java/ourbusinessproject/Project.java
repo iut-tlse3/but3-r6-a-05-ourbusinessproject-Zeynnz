5d4a3d36218405913cf744814dc85c8ffd2e6480
Zeynn <98015715+Zeynnz@users.noreply.github.com>
2026-01-27 10:45:07 +0100

    fix #1.1 classe Project

--- src/main/java/ourbusinessproject/Project.java
+++ src/main/java/ourbusinessproject/Project.java
@@ -1,22 +1,45 @@
 package ourbusinessproject;
 
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class Project {
 
-    @NotNull
-    @Size(min=1)
+    @NotEmpty
     private String title;
 
     private String description;
 
-
+    /**
+     * Set the title
+     * @param title the title
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     *
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * Set the description
+     * @param projectDescription the description
+     */
     public void setDescription(String projectDescription) {
         this.description = projectDescription;
+    }
+
+    /**
+     *
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
     }
 }
