b29ac7c5197e7d2170e7804838b9d78f4170c513
Zeynn <98015715+Zeynnz@users.noreply.github.com>
2026-01-27 08:50:27 +0100

    initial commit spring boot project

---
+++ src/main/java/ourbusinessproject/OurbusinessprojectApplication.java
@@ -0,0 +1,13 @@
+package ourbusinessproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurbusinessprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OurbusinessprojectApplication.class, args);
+    }
+
+}
