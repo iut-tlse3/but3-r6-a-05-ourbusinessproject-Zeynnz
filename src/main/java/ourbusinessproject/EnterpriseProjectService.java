a03236ca984c7d3866767474559e70371f071f16
Zeynn <98015715+Zeynnz@users.noreply.github.com>
2026-01-28 11:12:17 +0100

    fix #3.1 fetching projects with enterprises OK

--- src/main/java/ourbusinessproject/EnterpriseProjectService.java
+++ src/main/java/ourbusinessproject/EnterpriseProjectService.java
@@ -2,7 +2,10 @@ package ourbusinessproject;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EnterpriseProjectService {
@@ -86,5 +89,11 @@ public class EnterpriseProjectService {
         this.entityManager.persist(newEnterprise);
         this.entityManager.flush();
         return newEnterprise;
+    }
+
+    public List<Project> findAllProjects() {
+        String query = "SELECT p FROM Project p ORDER BY title";
+        TypedQuery<Project> queryObj = entityManager.createQuery(query, Project.class);
+        return  queryObj.getResultList();
     }
 }
