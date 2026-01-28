9a12e5e0f54178bbdcbc33469cb1eda53a4d6541
Zeynn <98015715+Zeynnz@users.noreply.github.com>
2026-01-28 09:01:19 +0100

    fix #2.2 relation Enterprise Project

--- src/main/java/ourbusinessproject/Enterprise.java
+++ src/main/java/ourbusinessproject/Enterprise.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.*;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 
 @Entity
 public class Enterprise {
@@ -117,7 +118,7 @@ public class Enterprise {
      */
     public void addProject(Project project) {
         if (this.projects == null){
-            this.projects = new ArrayList<>();
+            this.projects = new HashSet<>();
         }
         this.projects.add((project));
     }
