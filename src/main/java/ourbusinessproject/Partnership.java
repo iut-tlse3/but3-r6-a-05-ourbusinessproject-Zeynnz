0bf0aa18c86ff194d7676cd0d3ca2e128d09b849
Zeynn <98015715+Zeynnz@users.noreply.github.com>
2026-01-30 10:11:15 +0100

    fix #4.2 classe PartnershipService

--- src/main/java/ourbusinessproject/Partnership.java
+++ src/main/java/ourbusinessproject/Partnership.java
@@ -1,18 +1,25 @@
 package ourbusinessproject;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 
 import java.util.Date;
 
-
+@Entity
 public class Partnership {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
     @NotNull
     private Enterprise enterprise;
 
     @NotNull
     private Date creationDate;
 
+    @ManyToOne
     @NotNull
     private Project project;
 
@@ -27,4 +34,12 @@ public class Partnership {
     public void setProject(Project project) {
     this.project = project;
     }
+
+    public Enterprise getEnterprise() { return this.enterprise;}
+
+    public Date getCreationDate(){ return this.creationDate;}
+
+    public Project getProject() {return this.project;}
+
+    public Long getId() {return this.id;}
 }
---
+++ src/main/java/ourbusinessproject/PartnershipService.java
@@ -0,0 +1,33 @@
+package ourbusinessproject;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Service;
+import java.util.Date;
+
+
+@Service
+public class PartnershipService {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public Partnership newPartnership(Project project, Enterprise partnerEnterprise) {
+        Partnership newPartnership = new Partnership();
+        newPartnership.setCreationDate(new Date());
+        newPartnership.setProject(project);
+        newPartnership.setEnterprise(partnerEnterprise);
+        this.entityManager.persist(newPartnership);
+        this.entityManager.flush();
+        return newPartnership;
+    }
+
+
+    public Partnership findPartnershipById(Long id){
+        return entityManager.find(Partnership.class ,id);
+    }
+
+    public void remove(Partnership partnership) {
+        entityManager.remove(partnership);
+    }
+}
